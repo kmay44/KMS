a5f0710320201d15993109a9d30f309aea3f3176
Kadu <kadu.mayur44@gmail.com>
2019-05-31 14:08:02 +1000

    fixed styling

--- reactTutorialApp/App.js
+++ reactTutorialApp/App.js
@@ -13,68 +13,68 @@ import Butt from './components/Button';
 //import AlertTest from './components/Alert';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+ 	ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+ 	android:
+		'Double tap R on your keyboard to reload,\n' +
+		'Shake or press menu button for dev menu',
 });
 
 type Props = {};
 export default class App extends Component<Props> {
-  handler = () => {
-    console.log('Pressed the button');
-  }
+	handler = () => {
+		console.log('Pressed the button');
+	}
 
-  alertClicked = () => {
-    Alert.alert(
-      "Expiration in 1 month",
-      'Replenish Soon Queen',
-      [
-      {text: 'Later', onPress: () => console.log("later pressed")},
-      {
-        text: "Ok",
-        onPress: () => console.log('Ok Pressed'),
-        style: "cancel"
-      }
-      ],
-      {cancelable : false}
-    );
-  };
+	alertClicked = () => {
+	Alert.alert(
+		"Expiration in 1 month",
+		'Replenish Soon Queen',
+		[
+			{text: 'Later', onPress: () => console.log("later pressed")},
+			{
+				text: "Ok",
+				onPress: () => console.log('Ok Pressed'),
+				style: "cancel"
+				}
+		],
+			{cancelable : false}
+	);
+	};
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-        <Butt onButt={this.handler} />
-        <Button
-        onPress = {this.alertClicked}
-        style={styles.buttonStyle}
-        title="Click Me"
-        />
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View style={styles.container}>
+			<Text style={styles.welcome}>Welcome to React Native!</Text>
+			<Text style={styles.instructions}>To get started, edit App.js</Text>
+			<Text style={styles.instructions}>{instructions}</Text>
+			<Butt onButt={this.handler} />
+			<Button
+			onPress = {this.alertClicked}
+			style={styles.buttonStyle}
+			title="Click Me"
+			/>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF',
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10,
+	},
+	instructions: {
+		textAlign: 'center',
+		color: '#333333',
+		marginBottom: 5,
+	},
   buttonStyle: {
     fontSize: 20, 
     textAlign: "center",
